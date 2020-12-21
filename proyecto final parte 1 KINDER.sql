19bfe68d3bab792d96be75f51926d8d4fbfa6a0b
JorgeLuisBP <75709800+JorgeLuisBP@users.noreply.github.com>
2020-12-21 09:27:49 -0500

    Update proyecto final parte 1 KINDER.sql

--- proyecto final parte 1 KINDER.sql
+++ proyecto final parte 1 KINDER.sql
@@ -28,29 +28,27 @@ select e.ECédula as [Cedula de estudiante], e.ENombres as [Nombre de estudiante
 			
 
 ---SEGUNDA CONSULTA---
-----¿ESTUDIANTES REPRESENTADOS POR EL PAPÁ?-----
-select e.ECédula as [Cedula de estudiante], e.ENombres as [Nombre de estudiante], e.EApellidos [Apellido del estudiante],
-		e.EGénero as [Genero del estudiante], 
-		r.Rnombres as [Nombre del representante], r.Rapellidos as [Apellido del representante],
-		r.Rgénero as [Genero] , t.ParentescoDelRepresentado
-		from Estudiante as e inner join Representante as r 
-			on r.Rcédula=e.CedulaR inner join TipoDeRepresentante as t on t.Tcedula=r.Rcédula
-			where ParentescoDelRepresentado like 'PAPÁ'
+----MOSTRAR DATOS DEL ESTUDIANTE, CON LOS DATOS DEL DOCTOR, UN DIAGNOSTICOS DE LA CITA MEDICA, MEDICAMENTOS A TOMAR Y LA FECHA DE LA CITA
+select e.ENombres, e.EApellidos, e.ECédula, c.DcédulaC,d.Dnombre,d.Dapellido,d.Dnacionalidad , c.Diagnostico,c.Medicamentos, c.Fecha   
+			from Estudiante as e 
+			inner join [Cita Médica] as c on e.ECédula=c.EcédulaC 
+			inner join Doctor as d on d.Dcédula=c.DcédulaC
+
+
+
 
 ---TERCER CONSULTA
------	Cantidad de calificaciones regulares, buenas y excelentes en cada actividad por cada infante.----
-select * from Estudiante
-select * from ProgramaDeEducacion
+---CONSULTAR EL TOP 50% DE ESTUDIANTES MATRICULADOS EN LOS PROGRAMAS DE EDUCACION, MOSTRAR DATOS DEL ESTUDIANTE, DEL PROFESIONAL DE LA CLASE, EL CODIGO DE LA ACTIVIDAD. 
+select top 50  percent e.ECédula as CedulaDelAlumno, e.ENombres as NombreDelAlumno, e.EApellidos as ApellidoDelAlumno, e.Ecorreo as correo, 
+		e.EGénero as genero, r.Rcédula as CeduladelRepresentante, r.Rnombres as NombreDelRepresentante,
+		r.Rapellidos as ApellidoDelRepresentante, pe.CodigoA as codigoDelprograma ,a.nombre as NombreDelPrograma , 
+		p.ProfNombres as NombreDelProfesor, p.ProfApellidos as ApellidoDelProfesor
+			from ProgramaDeEducacion as pe inner join Estudiante as e on pe.CedulaEst=e.ECédula
+			inner join Representante as r on e.CedulaR=r.Rcédula
+			inner join Actividad as a on pe.CodigoA=a.codigo
+			inner join Profesional as p on pe.CédulaProf=p.ProfCédula
 
-select e.ECédula [Cedula del Estudiante],e.ENombres [Nombre Del Estudiante], e.EApellidos [Apellido Del Estudiante],Rendimiento, 
-		COUNT(Rendimiento) as CantidadDeCalificaciones
-		from ProgramaDeEducacion as pe inner join Estudiante as e on  e.ECédula=pe.CedulaEst 
-		group by Rendimiento, e.ECédula,e.ENombres, e.EApellidos 
 
-				---SUMATORIA GENERAL--
-				select Rendimiento, COUNT (Rendimiento)as CantidadDeCalificaciones
-				from ProgramaDeEducacion
-				group by Rendimiento 
 
 ---CUARTA CONSULTA--
 ----Mostrar datos del estudiante (nombre-apellidos-cedula-fecha de nacimiento) y calcular cuantos años actualmente tiene cada estudiante
@@ -66,18 +64,27 @@ select e.ECédula as CedulaDeEstudiante, e.ENombres as NombreDelEstudiante, e.EA
 		,DATEDIFF(mm, FechaDeNacimiento , GETDATE()) as Meses 
 		, DATEDIFF(dd, FechaDeNacimiento , GETDATE()) as Días 
 		from Estudiante as e
-----MOSTRAR DATOS DEL ESTUDIANTE, CON LOS DATOS DEL DOCTOR, UN DIAGNOSTICOS DE LA CITA MEDICA, MEDICAMENTOS A TOMAR Y LA FECHA DE LA CITA
-select e.ENombres, e.EApellidos, e.ECédula, c.DcédulaC,d.Dnombre,d.Dapellido,d.Dnacionalidad , c.Diagnostico,c.Medicamentos, c.Fecha   
-			from Estudiante as e 
-			inner join [Cita Médica] as c on e.ECédula=c.EcédulaC 
-			inner join Doctor as d on d.Dcédula=c.DcédulaC
 
----CONSULTAR EL 50% DE ESTUDIANTES MATRICULADOS EN LOS PROGRAMAS DE EDUCACION, MOSTRAR DATOS DEL ESTUDIANTE, DEL PROFESIONAL DE LA CLASE, EL CODIGO DE LA ACTIVIDAD. 
-select top 50  percent e.ECédula as CedulaDelAlumno, e.ENombres as NombreDelAlumno, e.EApellidos as ApellidoDelAlumno, e.Ecorreo as correo, 
-		e.EGénero as genero, r.Rcédula as CeduladelRepresentante, r.Rnombres as NombreDelRepresentante,
-		r.Rapellidos as ApellidoDelRepresentante, pe.CodigoA as codigoDelprograma ,a.nombre as NombreDelPrograma , 
-		p.ProfNombres as NombreDelProfesor, p.ProfApellidos as ApellidoDelProfesor
-			from ProgramaDeEducacion as pe inner join Estudiante as e on pe.CedulaEst=e.ECédula
-			inner join Representante as r on e.CedulaR=r.Rcédula
-			inner join Actividad as a on pe.CodigoA=a.codigo
-			inner join Profesional as p on pe.CédulaProf=p.ProfCédula
+----¿ESTUDIANTES REPRESENTADOS POR EL PAPÁ?-----
+select e.ECédula as [Cedula de estudiante], e.ENombres as [Nombre de estudiante], e.EApellidos [Apellido del estudiante],
+		e.EGénero as [Genero del estudiante], 
+		r.Rnombres as [Nombre del representante], r.Rapellidos as [Apellido del representante],
+		r.Rgénero as [Genero] , t.ParentescoDelRepresentado
+		from Estudiante as e inner join Representante as r 
+			on r.Rcédula=e.CedulaR inner join TipoDeRepresentante as t on t.Tcedula=r.Rcédula
+			where ParentescoDelRepresentado like 'PAPÁ'
+
+
+-----	Cantidad de calificaciones regulares, buenas y excelentes en cada actividad por cada infante.----
+select * from Estudiante
+select * from ProgramaDeEducacion
+
+select e.ECédula [Cedula del Estudiante],e.ENombres [Nombre Del Estudiante], e.EApellidos [Apellido Del Estudiante],Rendimiento, 
+		COUNT(Rendimiento) as CantidadDeCalificaciones
+		from ProgramaDeEducacion as pe inner join Estudiante as e on  e.ECédula=pe.CedulaEst 
+		group by Rendimiento, e.ECédula,e.ENombres, e.EApellidos 
+
+				---SUMATORIA GENERAL--
+				select Rendimiento, COUNT (Rendimiento)as CantidadDeCalificaciones
+				from ProgramaDeEducacion
+				group by Rendimiento 
